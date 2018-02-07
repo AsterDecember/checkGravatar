f3dd3575840426442f67c14843e2b8d1191158e3
AsterDecember <aster.december@gmail.com>
2018-02-06 21:45:14 -0600

    md5 on input and send it to image above to gravatar

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ console.log('js working!');
 function getInput() {
 	console.log('Info:');
 	var input = document.getElementById('input').value;
-	document.getElementById('inputText').innerHTML = input;
-	console.log(input);
+	var profile = md5(input.toLowerCase());
+	var url = 'https://www.gravatar.com/avatar/'+profile;
+	console.log(url);
+	document.getElementById('imgGravatar').src = url;
 }
